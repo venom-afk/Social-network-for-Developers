20e976e206c3fcb40b5e65f54404f7eab6838c4f
venom-afk <imammar1997@gmail.com>
2021-06-23 18:47:43 +0400

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
